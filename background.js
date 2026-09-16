c619df12dded7058ca154ede4846c11b0992cd41
kumikilongyeyo <klydesosa.art09@gmail.com>
2026-09-16 21:19:52 +0800

    Update install log for v0.6

--- background.js
+++ background.js
@@ -1,1 +1,1 @@
-chrome.runtime.onInstalled.addListener(() => console.log('Magic Poser Helper v0.5.2 installed'));
+chrome.runtime.onInstalled.addListener(() => console.log('Magic Poser Helper v0.6 installed'));
